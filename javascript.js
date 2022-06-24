c5b89c848e5ceee7b8aeece3a548f990921692ed
Nguyễn Tấn Hưng <92977878+nguyentanhungg@users.noreply.github.com>
2022-06-24 09:43:58 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function ten() {
+    document.getElementById("demo").innerHTML = "Nguyễn Tấn Hưng";
+  }
+function ngaysinh() {
+    document.getElementById("demo").innerHTML = "29/01/2003";
+  }
+function gioitinh() {
+    document.getElementById("demo").innerHTML = "Nam";
+  }
