d9a4c9944cfe74b51b2abc5cbb597ac254bc6bb9
Nguyễn Tấn Hưng <92977878+nguyentanhungg@users.noreply.github.com>
2022-07-07 22:46:48 +0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
-function ten() {
-    document.getElementById("demo").innerHTML = "Nguyễn Tấn Hưng";
-  }
-function ngaysinh() {
-    document.getElementById("demo").innerHTML = "29/01/2003";
-  }
-function gioitinh() {
-    document.getElementById("demo").innerHTML = "Nam";
-  }
+const form = document.getElementById('output').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const ten = document.getElementById('nhapten').value;
+    document.querySelector('.output-ten').textContent = `Tên: ${ten}`;
+
+    const email = document.getElementById('nhapemail').value;
+    document.querySelector('.output-email').textContent = `Email: ${email}`;
+
+    const genders = document.getElementsByName('gioitinh');
+    genders.forEach(gioitinh => {
+        if (gioitinh.checked == true) {
+            document.querySelector('.output-gioitinh').textContent = `Giới Tính: ${gioitinh.value}`;
+        };
+    });
+
+    const quoctich = document.getElementById('chonquoctich').value;
+    document.querySelector('.output-quoctich').textContent = `Quốc Tịch: ${quoctich}`;
+
+    const hobbies = document.getElementsByName('sothich');
+    sothichArray = [];
+    hobbies.forEach(sothich => {
+        if (sothich.checked == true) {
+            sothichArray.push(sothich.value);
+        };
+    });
+    document.querySelector('.output-sothich').textContent = `Sở Thích: ${sothichArray.join(', ')}`;
+});
